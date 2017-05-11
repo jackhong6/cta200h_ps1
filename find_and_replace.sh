4bb7d21f5f3df0affb24c3a94978103df35ed5c2
jackhong6 <jackhong6@gmail.com>
2017-05-11 12:43:17 -0400

    Working bash script for find and replace
    Problem 1 completed

--- find_and_replace.sh
+++ find_and_replace.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-if ! [ $# -eq 2 ]
-then
-    echo Usage: find_and_replace \<find\> \<replace\>
+if ! [ $# -eq 2 ]; then
+    echo Usage: ./find_and_replace \<find\> \<replace\>
     exit 1
 fi
 
-if [ -d "$replace" ]; then
-    mkdir replace
-fi
+mkdir -p replace
 
-sed 's/$1/$2/' *.txt > replace/out.txt
-echo $1; echo $2
+cp *.txt replace/
+cd replace
+
+sed -i .bak "s/$1/$2/g" *
+rm *.bak
+
+cd ..
